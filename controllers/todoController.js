f73d1883ff4fd2228dc22a3f3d3a8430cea732eb
Erin Dubuc <erin.dubuc9@gmail.com>
2018-09-17 17:19:40 +0000

    lesson36 connected to MongoDB with mongoose

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,4 +1,21 @@
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+
+// connect to the database
+mongoose.connect('mongodb://test:testing1@ds259802.mlab.com:59802/todo', {useNewUrlParser: true });
+
+// create a schema - this is like a blueprint for the data
+// what kind of info mongo will expect from our data
+var todoSchema = new mongoose.Schema({
+    item: String
+});
+// create a model
+var Todo = mongoose.model('Todo', todoSchema);
+var itemOne = Todo({item: 'get flowers'}).save(function(err){
+    if (err) throw err;
+    console.log('item saved');
+});
+
 var data = [{item: 'get milk'}, {item: 'walk dog'}, {item: 'kick some coding ass'}];
 var urlencodedParser = bodyParser.urlencoded({extended: false});
 module.exports = function(app){
